3d68c661cf2bfd41acca229490edae919e69db58
Olesia Kuklina <o.a.kuklina@yandex.ru>
2023-04-07 16:31:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#♫ Xylophone
+
+##Description
+Xylophone is a musical instrument that anyone can play! Just tap different colored bars on the screen and enjoy.
+This application was created during "The Complete iOS App Development Bootcamp". Its layout was created from scratch.
+
+![Test App](Documentation/Egg_timer.gif)
---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Lesya Kotty on 30.03.2023.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(button: sender)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(button: UIButton) {
+        let url = Bundle.main.url(forResource: button.currentTitle!, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
